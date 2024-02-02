a8572db94b37478d6fb3c4cae78ddb1b1fc2dc02
yangmei000 <liangzhe@liangzhedeMacBook-Pro.local>
2024-02-02 14:27:49 +0800

    commit message

--- model.py
+++ model.py
@@ -5,9 +5,9 @@ from xgboost import sklearn
 import numpy as np
 from sklearn import metrics
 
-data_read = pd.read_excel(r"细胞建模第二次数据  2023.9.24.xlsx")
+data_read = pd.read_excel(r"cell_model_build.xlsx")
 # print(data_read.columns)
-data_read.drop(["序号",'ID号'], axis=1, inplace=True)
+data_read.drop(["Serial number",'ID'], axis=1, inplace=True)
 new_columns = [i+"_"+j for i, j in zip(data_read.columns, data_read.iloc[0])]
 data_read.columns = new_columns
 data_read.drop(index=0, axis=0, inplace=True)
@@ -18,14 +18,15 @@ all_null_list = [i for i, j in zip(
 data_null = set(any_null_list).difference(all_null_list)
 for i in data_null:
     data_read[i].fillna(np.mean(data_read[i]), inplace=True)
-data_read['结局_症状持续时间；1≤7天；2>7'] = data_read['结局_症状持续时间；1≤7天；2>7'].astype(
+print(data_read.columns)
+data_read['Duration of symptoms_1≤7days;\n2>7 days.'] = data_read['Duration of symptoms_1≤7days;\n2>7 days.'].astype(
     'category')
-data_read['结局_症状持续时间；1≤7天；2>7'] = data_read['结局_症状持续时间；1≤7天；2>7'].apply(
+data_read['Duration of symptoms_1≤7days;\n2>7 days.'] = data_read['Duration of symptoms_1≤7days;\n2>7 days.'].apply(
     lambda x: 0 if x == 1 else x)
-data_read['结局_症状持续时间；1≤7天；2>7'] = data_read['结局_症状持续时间；1≤7天；2>7'].apply(
+data_read['Duration of symptoms_1≤7days;\n2>7 days.'] = data_read['Duration of symptoms_1≤7days;\n2>7 days.'].apply(
     lambda x: 1 if x == 2 else x)
-y = data_read.get('结局_症状持续时间；1≤7天；2>7')
-X = data_read.drop(['结局_症状持续时间；1≤7天；2>7'], axis=1)
+y = data_read.get('Duration of symptoms_1≤7days;\n2>7 days.')
+X = data_read.drop(['Duration of symptoms_1≤7days;\n2>7 days.'], axis=1)
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=30)#30
 X_train, X_test, y_train, y_test = x_train.values, x_test.values, y_train.values, y_test.values
 eval_set = [(X_train, y_train), (X_test, y_test)]
@@ -47,7 +48,7 @@ precision = metrics.precision_score(y_test, y_predict)
 recall = metrics.recall_score(y_test, y_predict)
 f1 = metrics.f1_score(y_test, y_predict)
 matrix = metrics.confusion_matrix(y_test, y_predict)
-print("总体准确率为"+str(acc))
-print("精确率"+str(precision))
-print("召回率"+str(recall))
-print("f1值"+str(f1))
+print("Accuracy:"+str(acc))
+print("Precision:"+str(precision))
+print("Recall:"+str(recall))
+print("F1-sorce:"+str(f1))
